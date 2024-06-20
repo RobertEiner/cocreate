7e16d53fcf5ebeee7133d888bed19554be147ef1
Robert <guseinero@student.gu.se>
2024-06-20 16:50:41 +0200

    Add files for Comment entity

--- src/main/java/com/cocreate/comment/Comment.java
+++ src/main/java/com/cocreate/comment/Comment.java
@@ -1,4 +1,20 @@
 package com.cocreate.comment;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Comment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int commentId;
+
+
+
+
+
+
 }
