7ee96f9504617e460326f98b87217fd58fc0eaf1
Robert <guseinero@student.gu.se>
2024-07-25 11:10:25 +0200

    #7 Add exception handler for empty fields

--- src/main/java/com/cocreate/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/cocreate/exceptions/GlobalExceptionHandler.java
@@ -1,13 +1,17 @@
 package com.cocreate.exceptions;
 
+import jakarta.validation.ConstraintViolationException;
+import org.apache.coyote.Response;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.sql.SQLIntegrityConstraintViolationException;
 
-@ControllerAdvice
+@RestControllerAdvice // This annotation is used to denote a class that will be called whenever an exception is thrown
 public class GlobalExceptionHandler {
 
 
@@ -17,10 +21,28 @@ public class GlobalExceptionHandler {
         return new ResponseEntity<>("Username and email needs to be unique\n" + e.getMessage(), HttpStatus.BAD_REQUEST);
     }
 
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<String> handleConstraintViolationException(Exception e) {
+        return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<String> handleMethodArgumentNotValidException(Exception e) {
+        return new ResponseEntity<>(e.getMessage(),HttpStatus.BAD_REQUEST);
+    }
+
+    
+    /**
+     * Custom exceptions
+     * */
+
+    // When a resource such as a post or developer couldn't be found based on the ID
     @ExceptionHandler(ResourceNotFoundException.class)
     public ResponseEntity<String> handleResourceNotFoundException(ResourceNotFoundException e) {
         return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
     }
+
 
 
 }
